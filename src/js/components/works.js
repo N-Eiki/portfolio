493056458b3e4143d95ccc91ae89f60a6b4d567b
N-Eiki <naganagaeichan@gmail.com>
2020-07-14 22:18:12 +0900

    modify text

--- src/js/components/works.js
+++ src/js/components/works.js
@@ -7,7 +7,7 @@ export class Works extends React.Component{
             <div id="works">
                 <h1>works</h1>
                 <h2><a href="https://github.com/N-Eiki/portfolio">ポートフォリオ</a></h2>
-                <p className="text">このポートフォリのページのことです。このサイトはReactで書いていて、GitHubで公開しています</p>
+                <p className="text">このポートフォリのページのことです。このサイトはReactで書いていて、GitHub Pagesを用いて公開しています</p>
                 <h2><a href="https://github.com/N-Eiki/favoritebook">Faborite Book</a></h2>
                 <p className="text">このアプリはDjangoを用いて作成しました。詳しくはリンク先からご覧ください。</p>
             </div>
